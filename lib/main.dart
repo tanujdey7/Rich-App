f92d26571e8999a6a2211a397edf3a2e3f01f876
Tanuj Dey <tanujdey7@gmail.com>
2020-04-28 19:33:59 +0530

    v1.0

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+/*
+*/
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[400],
+        appBar: AppBar(
+          title: Text("I am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
